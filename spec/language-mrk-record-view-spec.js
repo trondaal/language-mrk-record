c8a828c7e9e94c7f80c678e061b4a32ac17931dc
Trond Aalberg <trond.aalberg@ntnu.no>
2022-05-04 20:38:47 +0200

    Initial commit

---
+++ spec/language-mrk-record-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageMrkRecordView from '../lib/language-mrk-record-view';
+
+describe('LanguageMrkRecordView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
